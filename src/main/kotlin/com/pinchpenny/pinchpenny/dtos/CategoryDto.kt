bfc489b4e3f2740ef03ecb74d7df588eaa508fcb
hamzaraza-10p <hamza.raza@10pearls.com>
2023-02-20 15:04:00 +0500

    Added Category DTO

--- src/main/kotlin/com/pinchpenny/pinchpenny/dtos/CategoryDto.kt
+++ src/main/kotlin/com/pinchpenny/pinchpenny/dtos/CategoryDto.kt
@@ -1,18 +1,32 @@
 package com.pinchpenny.pinchpenny.dtos
 
+import com.pinchpenny.pinchpenny.modals.Category
+
 data class CategoryCreateRequest(
     val title:String,
     val mainId:Long?,
     val imageUrl:String,
     val type:String,
 )
-
+fun CategoryCreateRequest.toEntity() = Category(
+    title = title,
+    mainId = mainId!!,
+    imageUrl = imageUrl,
+    type = type
+)
 data class CategoryUpdateRequest(
     val id:Long,
     val title:String?,
     val mainId:Long?,
     val imageUrl:String?,
     val type:String?,
+)
+fun CategoryUpdateRequest.toEntity() = Category(
+    id = id,
+    title = title!!,
+    mainId = mainId!!,
+    imageUrl = imageUrl!!,
+    type = type!!
 )
 
 data class  CategoryDeleteRequest(
@@ -24,4 +38,10 @@ data class CategoryResponse(
     val mainId:Long,
     val imageUrl:String,
     val type:String,
+)
+fun CategoryResponse.toModel() = CategoryResponse(
+    title = title!!,
+    mainId = mainId!!,
+    imageUrl = imageUrl!!,
+    type = type!!
 )
