65ccf09d3c9ff016fbdc2c7bf884121ffa20f546
Ali-Hasnain-17 <--replace-all>
2023-02-22 13:22:35 +0500

    temp

--- src/main/kotlin/com/pinchpenny/pinchpenny/modals/Category.kt
+++ src/main/kotlin/com/pinchpenny/pinchpenny/modals/Category.kt
@@ -17,14 +17,12 @@ import java.util.UUID
 @Builder
 data class Category (
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private val id: UUID? = null,
-    @Column(name = "main_id")
-    private var mainId: UUID,
+    @GeneratedValue(strategy = GenerationType.AUTO) val id: UUID? = null,
+    @Column(name = "main_id") var mainId: UUID,
     @Column(name="image_url")
     private var imageUrl: String,
     private var type: String,
-    private var title: String,
+    var title: String,
     @ManyToMany(mappedBy = "categories")
     @JsonIgnoreProperties("categories")
     private var transactions:List<Transaction> = mutableListOf()
